abada1f28177ff4c5dd1eddc9a3007f6fc181aed
cohitt01 <cohitt01@wsc.edu>
2025-09-17 10:31:49 -0500

    tried putting in random number calculator, and used if/else to see if correct.

--- main.cpp
+++ main.cpp
@@ -10,22 +10,35 @@ Program......: MathTutorV2
 #include <iostream>
 using namespace std;
 
-int main() {
+int termain() {
   string userName;
   int userInput;
   int userAnswer;
-    cout << "What's your name? :";
+    cout << "What's your name?:";
     cin >> userName; // collects first name
     cout << "*  Welcome " << userName << " to the The Super Simple Math Tutor  *" << endl; //Greeting
     cout << endl;
     cout << "                PRESS ENTER TO BEGIN" << endl;
-    _getwch(); //has person press any key before continuing (found by AI)
-
+    _getwch(); //has person press en key before continuing (found by AI)
+        //Failed acual random number Generagtion.
     srand(time(NULL));
     int userAwnser;
-        cout << rand() % 100 + 1 << " + " << rand() % 100 +1;
+    int leftNum = rand() % 100 + 1;
+    int rightNum = rand() % 100 + 1;
+
+    cout << leftNum << " + " << rightNum << " = ";
         cin >> userAnswer;
-    if (userAnswer ==
+    if(userAnswer == leftNum + rightNum) { //seeing if userAnswer is correct
+        cout << userAnswer << " is correct." << endl;
+        cout << endl;
+        cout << "Lets do another." << endl;
+        cout << leftNum << " + " << rightNum << " =";
+    }
+    else {          //if userAnswer wrong
+        cout << userAnswer << " is incorrect" << endl;
+        cout << "Correct Answer is " << leftNum + rightNum << endl;
+        cout << "Lets do another to practice." << endl;
+    }
 
 return 0;
 }
